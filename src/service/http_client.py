bd5028c7ae7f2117ede7b6e974e661087204e8e0
Rafael Gonzalez <rafael.gonzalez@base22.com>
2021-04-05 18:11:57 -0500

    Remove prints

--- src/service/http_client.py
+++ src/service/http_client.py
@@ -8,7 +8,5 @@ def get(url):
 
 def post(url, payload):
     headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-    print("Payload")
-    print(payload)
     r = requests.post(url, json=payload, headers=headers)
     return r.json() if r.status_code == requests.codes.ok else r.text
