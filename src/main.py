340e67a81dee8df1135fc0f2ae9e6dd42d4abfb7
Rafael Gonzalez <rafael.gonzalez@base22.com>
2021-04-06 13:59:38 -0500

    Integrate Storage API services

--- src/main.py
+++ src/main.py
@@ -38,14 +38,19 @@ pieces_of_content = parse_pieces_of_content(EXCEL_PATH)
 json_data = json.dumps(pieces_of_content)
 
 TemplateNames = html_markup_generator.TemplateNames
+
+authoringTemplateName = "CNH_File"
+contentLibraryName = "Web Content"
+path = "test"
+
 kit1 = HTMLElement("Google", tag_name="a", attrs={"href": "https://www.google.com"})
 kit2 = HTMLElement("Facebook", tag_name="a", attrs={"href": "https://www.facebook.com"})
 kit3 = HTMLElement("Instagram", tag_name="a", attrs={"href": "https://www.instagram.com"})
 kit4 = HTMLElement("Apple", tag_name="a", attrs={"href": "https://www.apple.com"})
-post = Resource(name="post-3", title="Post 3", authoringTemplateName="CNH_File",
-                contentLibraryName="Web Content", path="test")
-post2 = Resource(name="post-4", title="Post 4", authoringTemplateName="CNH_File",
-                 contentLibraryName="Web Content", path="test")
+post = Resource(name="post-7", title="Post 7", authoringTemplateName=authoringTemplateName,
+                contentLibraryName=contentLibraryName, path=path)
+post2 = Resource(name="post-8", title="Post 8", authoringTemplateName=authoringTemplateName,
+                 contentLibraryName=contentLibraryName, path=path)
 response_post_1 = importer_service.save_item(post.to_dict())
 response_post_2 = importer_service.save_item(post2.to_dict())
 result_post_1 = utils.get_result(response_post_1)
@@ -56,13 +61,18 @@ related_post_1 = HTMLElement(result_post_1.get("title", ""), tag_name="a",
 related_post_2 = HTMLElement(result_post_2.get("title", ""), tag_name="a",
                              attrs={"href": utils.create_websphere_link(result_post_2.get("newId", ""),
                                                                         result_post_2.get("path", ""))})
+kit_file1 = HTMLElement("ADAM - Quick Guide 1 0", tag_name="a",
+                        attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "ADAM - Quick Guide 1 0.pdf"})
+kit_file2 = HTMLElement("DP_home.html", tag_name="a",
+                        attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "DP_home.html"})
 collapsible_elements = [CollapsibleElement("My first title", [kit1, kit2]),
                         CollapsibleElement("My second title", [kit3, kit4]),
-                        CollapsibleElement("Related Content", [related_post_1, related_post_2])]
+                        CollapsibleElement("Related Content", [related_post_1, related_post_2]),
+                        CollapsibleElement("LGF Space", [kit_file1, kit_file2])]
 campaign_body = CampaignHTMLBodyTemplate("Awesome Description", "<pre>This the WYSIWYG</pre>", collapsible_elements)
 page = html_markup_generator.generate(campaign_body, template_name=TemplateNames.CAMPAIGN.value)
-campaign = Resource(name="campaign-9", title="Campaign 9", authoringTemplateName="CNH_File",
-                    contentLibraryName="Web Content", path="test", description=str(page))
+campaign = Resource(name="campaign-11", title="Campaign 11", authoringTemplateName=authoringTemplateName,
+                    contentLibraryName=contentLibraryName, path=path, description=str(page))
 print(json_data)
 print("-------------------------------------------")
 print(page)
