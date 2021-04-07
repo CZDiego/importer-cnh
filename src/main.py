52c19fb15c1805d9334a870dc2306f3178b2b5a5
Rafael Gonzalez <rafael.gonzalez@base22.com>
2021-04-07 18:24:34 -0500

    Add mapping example

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # Run like:
 # python3 excel.py
-
+import logging
 import pandas
 import json
 from variables import *
@@ -8,13 +8,14 @@ import html_markup_utils.html_markup_generator as html_markup_generator
 import service.importer_service as importer_service
 from models import CollapsibleElement, Resource, HTMLElement, CampaignHTMLBodyTemplate
 import utils
+import sys
 
+sys.tracebacklimit = 0
 # TODO: Read excel file from local volume instead of having it in docker container
 EXCEL_PATH = r'/export-content-20210302121846.xlsx'
 
 
 def is_json_serializable(value):
-
     try:
         json.dumps(value)
         return True
@@ -61,35 +62,44 @@ authoringTemplateName = "CNH_File"
 contentLibraryName = "Web Content"
 path = "test"
 
-kit1 = HTMLElement("Google", tag_name="a", attrs={"href": "https://www.google.com"})
-kit2 = HTMLElement("Facebook", tag_name="a", attrs={"href": "https://www.facebook.com"})
-kit3 = HTMLElement("Instagram", tag_name="a", attrs={"href": "https://www.instagram.com"})
-kit4 = HTMLElement("Apple", tag_name="a", attrs={"href": "https://www.apple.com"})
-post = Resource(name="post-7", title="Post 7", authoringTemplateName=authoringTemplateName,
-                contentLibraryName=contentLibraryName, path=path)
-post2 = Resource(name="post-8", title="Post 8", authoringTemplateName=authoringTemplateName,
-                 contentLibraryName=contentLibraryName, path=path)
-result_post_1 = importer_service.save_item(post.to_dict())
-result_post_2 = importer_service.save_item(post2.to_dict())
-related_post_1 = HTMLElement(result_post_1.get("title", ""), tag_name="a",
-                             attrs={"href": utils.create_websphere_link(result_post_1.get("newId", ""),
-                                                                        result_post_1.get("path", ""))})
-related_post_2 = HTMLElement(result_post_2.get("title", ""), tag_name="a",
-                             attrs={"href": utils.create_websphere_link(result_post_2.get("newId", ""),
-                                                                        result_post_2.get("path", ""))})
-kit_file1 = HTMLElement("ADAM - Quick Guide 1 0", tag_name="a",
-                        attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "ADAM - Quick Guide 1 0.pdf"})
-kit_file2 = HTMLElement("DP_home.html", tag_name="a",
-                        attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "DP_home.html"})
-collapsible_elements = [CollapsibleElement("My first title", [kit1, kit2]),
-                        CollapsibleElement("My second title", [kit3, kit4]),
-                        CollapsibleElement("Related Content", [related_post_1, related_post_2]),
-                        CollapsibleElement("LGF Space", [kit_file1, kit_file2])]
-campaign_body = CampaignHTMLBodyTemplate("Awesome Description", "<pre>This the WYSIWYG</pre>", collapsible_elements)
-page = html_markup_generator.generate(campaign_body, template_name=TemplateNames.CAMPAIGN.value)
-campaign = Resource(name="campaign-11", title="Campaign 11", authoringTemplateName=authoringTemplateName,
-                    contentLibraryName=contentLibraryName, path=path, description=str(page))
+try:
+    kit1 = HTMLElement("Google", tag_name="a", attrs={"href": "https://www.google.com"})
+    kit2 = HTMLElement("Facebook", tag_name="a", attrs={"href": "https://www.facebook.com"})
+    kit3 = HTMLElement("Instagram", tag_name="a", attrs={"href": "https://www.instagram.com"})
+    kit4 = HTMLElement("Apple", tag_name="a", attrs={"href": "https://www.apple.com"})
+    post = Resource(name="post-7", title="Post 7", authoringTemplateName=authoringTemplateName,
+                    contentLibraryName=contentLibraryName, path=path)
+    post2 = Resource(name="post-8", title="Post 8", authoringTemplateName=authoringTemplateName,
+                     contentLibraryName=contentLibraryName, path=path)
+    result_post_1 = importer_service.save_item(post.to_dict())
+    result_post_2 = importer_service.save_item(post2.to_dict())
+    related_post_1 = HTMLElement(result_post_1.get("title", ""), tag_name="a",
+                                 attrs={"href": utils.create_websphere_link(result_post_1.get("newId", ""),
+                                                                            result_post_1.get("path", ""))})
+    related_post_2 = HTMLElement(result_post_2.get("title", ""), tag_name="a",
+                                 attrs={"href": utils.create_websphere_link(result_post_2.get("newId", ""),
+                                                                            result_post_2.get("path", ""))})
+    kit_file1 = HTMLElement("ADAM - Quick Guide 1 0", tag_name="a",
+                            attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "ADAM - Quick Guide 1 0.pdf"})
+    kit_file2 = HTMLElement("DP_home.html", tag_name="a",
+                            attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "DP_home.html"})
+    collapsible_elements = [CollapsibleElement("My first title", [kit1, kit2]),
+                            CollapsibleElement("My second title", [kit3, kit4]),
+                            CollapsibleElement("Related Content", [related_post_1, related_post_2]),
+                            CollapsibleElement("LGF Space", [kit_file1, kit_file2])]
+    campaign_body = CampaignHTMLBodyTemplate("Awesome Description", "<pre>This the WYSIWYG</pre>", collapsible_elements)
+    page = html_markup_generator.generate(campaign_body, template_name=TemplateNames.CAMPAIGN.value)
+    campaign = Resource(name="campaign-11", title="Campaign 11", authoringTemplateName=authoringTemplateName,
+                        contentLibraryName=contentLibraryName, path=path, description=str(page))
+    print("-------------------------------------------")
+    print(page)
+    result = importer_service.save_item(campaign.to_dict())
+    print(result)
+except (ConnectionError, Exception) as e:
+    logging.exception(e)
 
-print(page)
-result = importer_service.save_item(campaign.to_dict())
-print(result)
+print(utils.get_mapped_value("cih"))
+print(utils.get_mapped_value("ADVANCED FARMING SYSTEMS"))
+print(utils.get_mapped_value("UK"))
+
+print(json_data)
