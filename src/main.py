4430eb36dcc18f259ff4f303e20200d94f6c267e
Rafael Gonzalez <rafael.gonzalez@base22.com>
2021-04-08 14:35:44 -0500

    Change server for testing

--- src/main.py
+++ src/main.py
@@ -21,39 +21,47 @@ print("-------------------------------------------")
 
 TemplateNames = html_markup_generator.TemplateNames
 
-authoringTemplateName = "CNH_File"
-contentLibraryName = "Web Content"
-path = "test"
+authoringTemplateName = "Resource"
+contentLibraryName = "content-english"
+path = "cnhi/discover/internal/resources"
 
 try:
-    kit1 = HTMLElement("Google", tag_name="a", attrs={"href": "https://www.google.com"})
-    kit2 = HTMLElement("Facebook", tag_name="a", attrs={"href": "https://www.facebook.com"})
-    kit3 = HTMLElement("Instagram", tag_name="a", attrs={"href": "https://www.instagram.com"})
-    kit4 = HTMLElement("Apple", tag_name="a", attrs={"href": "https://www.apple.com"})
-    post = Resource(name="post-7", title="Post 7", authoringTemplateName=authoringTemplateName,
-                    contentLibraryName=contentLibraryName, path=path)
-    post2 = Resource(name="post-8", title="Post 8", authoringTemplateName=authoringTemplateName,
-                     contentLibraryName=contentLibraryName, path=path)
+    kit1 = HTMLElement("a", text="Google", attrs={"href": "https://www.google.com"})
+    kit2 = HTMLElement("a", text="Facebook", attrs={"href": "https://www.facebook.com"})
+    kit3 = HTMLElement("a", text="Instagram", attrs={"href": "https://www.instagram.com"})
+    kit4 = HTMLElement("a", text="Apple", attrs={"href": "https://www.apple.com"})
+    post = Resource(name="post-1", title="Post 1", authoringTemplateName=authoringTemplateName,
+                    contentLibraryName=contentLibraryName, path=path, transformHeadersH3="Collapsible Sections")
+    post2 = Resource(name="post-2", title="Post 2", authoringTemplateName=authoringTemplateName,
+                     contentLibraryName=contentLibraryName, path=path, transformHeadersH3="Collapsible Sections")
     result_post_1 = importer_service.save_item(post.to_dict())
     result_post_2 = importer_service.save_item(post2.to_dict())
-    related_post_1 = HTMLElement(result_post_1.get("title", ""), tag_name="a",
+    related_post_1 = HTMLElement("a", text=result_post_1.get("title", ""),
                                  attrs={"href": utils.create_websphere_link(result_post_1.get("newId", ""),
                                                                             result_post_1.get("path", ""))})
-    related_post_2 = HTMLElement(result_post_2.get("title", ""), tag_name="a",
+    related_post_2 = HTMLElement("a", text=result_post_2.get("title", ""),
                                  attrs={"href": utils.create_websphere_link(result_post_2.get("newId", ""),
                                                                             result_post_2.get("path", ""))})
-    kit_file1 = HTMLElement("ADAM - Quick Guide 1 0", tag_name="a",
-                            attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "ADAM - Quick Guide 1 0.pdf"})
-    kit_file2 = HTMLElement("DP_home.html", tag_name="a",
-                            attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "DP_home.html"})
+    kit_file1 = HTMLElement("a", text="20201208 CanadaContent (2) (1).json",
+                            attrs={"href": WEBSPHERE_VARIABLES.get(
+                                "StorageAPIBaseURL") + "20201208 CanadaContent (2) (1).json",
+                                   "target": "_blank"})
+    kit_file2 = HTMLElement("a", text="tbio_config (1).jsp",
+                            attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "tbio_config (1).jsp",
+                                   "target": "_blank"})
     collapsible_elements = [CollapsibleElement("My first title", [kit1, kit2]),
                             CollapsibleElement("My second title", [kit3, kit4]),
                             CollapsibleElement("Related Content", [related_post_1, related_post_2]),
                             CollapsibleElement("LGF Space", [kit_file1, kit_file2])]
+    downloads = html_markup_generator.create_rich_text([kit_file1, kit_file2])
+    related_content = html_markup_generator.create_rich_text([related_post_1, related_post_2])
+    print("related_content")
+    print(related_content)
     campaign_body = CampaignHTMLBodyTemplate("Awesome Description", "<pre>This the WYSIWYG</pre>", collapsible_elements)
     page = html_markup_generator.generate(campaign_body, template_name=TemplateNames.CAMPAIGN.value)
-    campaign = Resource(name="campaign-11", title="Campaign 11", authoringTemplateName=authoringTemplateName,
-                        contentLibraryName=contentLibraryName, path=path, description=str(page))
+    campaign = Resource(name="campaign-1", title="Campaign 1", authoringTemplateName=authoringTemplateName,
+                        contentLibraryName=contentLibraryName, path=path, body=str(page),
+                        transformHeadersH3="Collapsible Sections", downloads=downloads, relatedContent=related_content)
     print("-------------------------------------------")
     print(page)
     result = importer_service.save_item(campaign.to_dict())
