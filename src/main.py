d79a0192443d1f00734fd255b135bda9136dfa5e
Rafael Gonzalez <rafael.gonzalez@base22.com>
2021-04-20 15:28:36 -0500

    Remove comments

--- src/main.py
+++ src/main.py
@@ -33,7 +33,6 @@ pieces_of_content = preprocessing.get_pieces_of_content(EXCEL_PATH)
 json_array = []
 
 for i in range(0, len(pieces_of_content)):
-    # json_array.append(pieces_of_content[i].__dict__)
     json_array.append(utils.remove_nones_from_dict(pieces_of_content[i].__dict__))
 
 
@@ -196,77 +195,4 @@ timestamp = str(now.timestamp())
 file_path = base_dir + "generated_json_" + timestamp + ".json"
 logger.info("Creating json file in " + file_path)
 write_file(file_path, content)
-# init_migration(json_array)
-
-# logger.info(json_data)
-"""
-TemplateNames = html_markup_generator.TemplateNames
-
-authoringTemplateName = "Resource"
-contentLibraryName = "content-english"
-path = "cnhi/discover/internal/resources"
-
-try:
-    kit1 = HTMLElement("a", text="Google", attrs={"href": "https://www.google.com"})
-    kit2 = HTMLElement("a", text="Facebook", attrs={"href": "https://www.facebook.com"})
-    kit3 = HTMLElement("a", text="Instagram", attrs={"href": "https://www.instagram.com"})
-    kit4 = HTMLElement("a", text="Apple", attrs={"href": "https://www.apple.com"})
-    post = Resource(name="post-1", title="Post 1", authoringTemplateName=authoringTemplateName,
-                    contentLibraryName=contentLibraryName, path=path,
-                    transformHeadersH3=TransformHeaders.COLLAPSIBLE_SECTIONS.value)
-    post2 = Resource(name="post-2", title="Post 2", authoringTemplateName=authoringTemplateName,
-                     contentLibraryName=contentLibraryName, path=path,
-                     transformHeadersH3=TransformHeaders.COLLAPSIBLE_SECTIONS.value)
-    result_post_1 = importer_service.save_item(post.to_dict())
-    result_post_2 = importer_service.save_item(post2.to_dict())
-    related_post_1 = HTMLElement("a", text=result_post_1.get("title", ""),
-                                 attrs={"href": utils.create_websphere_link(result_post_1.get("newId", ""),
-                                                                            result_post_1.get("path", ""))})
-    related_post_2 = HTMLElement("a", text=result_post_2.get("title", ""),
-                                 attrs={"href": utils.create_websphere_link(result_post_2.get("newId", ""),
-                                                                            result_post_2.get("path", ""))})
-    kit_file1 = HTMLElement("a", text="20201208 CanadaContent (2) (1).json",
-                            attrs={"href": WEBSPHERE_VARIABLES.get(
-                                "StorageAPIBaseURL") + "20201208 CanadaContent (2) (1).json",
-                                   "target": "_blank"})
-    kit_file2 = HTMLElement("a", text="tbio_config (1).jsp",
-                            attrs={"href": WEBSPHERE_VARIABLES.get("StorageAPIBaseURL") + "tbio_config (1).jsp",
-                                   "target": "_blank"})
-    collapsible_elements = [CollapsibleElement("My first title", [kit1, kit2]),
-                            CollapsibleElement("My second title", [kit3, kit4]),
-                            CollapsibleElement("Related Content", [related_post_1, related_post_2]),
-                            CollapsibleElement("LGF Space", [kit_file1, kit_file2])]
-    downloads = html_markup_generator.create_rich_text([kit_file1, kit_file2])
-    related_content = html_markup_generator.create_rich_text([related_post_1, related_post_2])
-    logger.info("related_content")
-    logger.info(related_content)
-    campaign_body = CampaignHTMLBodyTemplate("Awesome Description", "<pre>This the WYSIWYG</pre>", collapsible_elements)
-    page = html_markup_generator.generate(campaign_body, template_name=TemplateNames.CAMPAIGN.value)
-    campaign = Resource(name="campaign-1", title="Campaign 1", authoringTemplateName=authoringTemplateName,
-                        contentLibraryName=contentLibraryName, path=path, body=str(page),
-                        transformHeadersH3=TransformHeaders.COLLAPSIBLE_SECTIONS.value, downloads=downloads,
-                        relatedContent=related_content, dealershipTypeVisibility=",".join(
-            [DealershipTypes.DEALER.value, DealershipTypes.SUB_DEALER.value]))
-    logger.info("-------------------------------------------")
-    logger.info(page)
-    result = importer_service.save_item(campaign.to_dict())
-    logger.info(result)
-except (ConnectionError, Exception) as e:
-    logging.exception(e)
-
-logger.info(utils.get_mapped_value("cih"))
-logger.info(utils.get_mapped_value("ADVANCED FARMING SYSTEMS"))
-logger.info(utils.get_mapped_value("UK"))
-
-# logger.info(json_data)
-
-
-div = HTMLElement("div")
-bannerTitle = HTMLElement("span", "Facebook Banners")
-hr = HTMLElement("hr")
-banner1 = HTMLElement("a", "Facebook Banner 1", dict(href="#"))
-banner2 = HTMLElement("a", "Facebook Banner 2", dict(href="#"))
-
-downloads = html_markup_generator.create_rich_text([div, bannerTitle, hr, banner1, banner2])
-logger.info(downloads)
-"""
+init_migration(json_array)
